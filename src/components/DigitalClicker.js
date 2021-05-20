99620f34c1572836f0df750351bb85c39bf9658b
Todd Stoneberg <todd.stoneberg@gmail.com>
2021-05-20 16:21:40 -0600

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,21 @@
 // Code DigitalClicker Component Here
+import React from 'react' 
+
+class DigitalClicker extends React.Component {
+
+    state = {
+        timesClicked: 0
+    }
+
+    increase = () => {
+        this.setState(previousState => {
+            return { timesClicked: previousState.timesClicked + 1 }
+        })
+    }
+
+    render() {
+        return <button onClick={this.increase}>{this.state.timesClicked}</button>
+    }
+}
+
+export default DigitalClicker
